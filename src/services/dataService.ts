da539d3939063f2d7adb930aca0a7bbd0c637ec8
ptaverroesvisionarydesign-cyber <ptaverroesvisionarydesign@gmail.com>
2026-04-29 19:12:55 +0700

    Update dataService.ts

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -6,6 +6,7 @@
 import { GoogleGenAI } from "@google/genai";
 import { DataPelakuUsaha, StatusProses, UserRole, User, Kbli, Wilayah } from "../types";
 import { format } from "date-fns";
+console.log("ENV:", import.meta.env);
 console.log("API KEY:", import.meta.env.VITE_API_KEY);
 const ai = new GoogleGenAI({
   apiKey: import.meta.env.VITE_API_KEY
