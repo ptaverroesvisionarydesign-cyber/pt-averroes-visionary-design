866ba387685abd3cbab7c10385286866ca61e768
ptaverroesvisionarydesign-cyber <ptaverroesvisionarydesign@gmail.com>
2026-05-01 19:27:11 +0700

    Update Firebase configuration to inline object
    Replaced firebaseConfig import with inline configuration.

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,9 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import firebaseConfig from '../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpzGdKl85x-ZT-AR5Cd_2mOCowWIc8aeI", // isi dari Firebase
+  authDomain: "xxxx.firebaseapp.com",
+  projectId: "xxxx",
+  storageBucket: "xxxx.appspot.com",
+  messagingSenderId: "xxxx",
+  appId: "xxxx"
+};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
