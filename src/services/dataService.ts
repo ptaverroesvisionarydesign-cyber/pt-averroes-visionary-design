3426dfe5bfb1500c03e88905a2ed202cc562d85f
ptaverroesvisionarydesign-cyber <ptaverroesvisionarydesign@gmail.com>
2026-05-04 17:52:15 +0700

    Update dataService.ts

--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -47,7 +47,6 @@ function handleFirestoreError(error: unknown, operationType: OperationType, path
   console.error('Firestore Error: ', JSON.stringify(errInfo));
   throw new Error(JSON.stringify(errInfo));
 }
-
 console.log("ENV:", import.meta.env);
 console.log("API KEY:", import.meta.env.VITE_API_KEY);
 const ai = new GoogleGenAI({
